d389c2f7d125644aa19273364ccaa96395b539a5
Ayush Goyal <ayush987goyal@gmail.com>
2018-04-15 23:59:58 +0530

    added initial client side with service worker

---
+++ client/main.js
@@ -0,0 +1,27 @@
+// Service Worker Registration
+let swReg;
+
+// Update UI for subscribed state
+const setSubscribedStatus = state => {
+  if (state) {
+    document.getElementById('subscribe').className = 'hidden';
+    document.getElementById('unsubscribe').className = '';
+  } else {
+    document.getElementById('subscribe').className = '';
+    document.getElementById('unsubscribe').className = 'hidden';
+  }
+};
+
+// Register Service Worker
+if (navigator.serviceWorker) {
+  navigator.serviceWorker
+    .register('sw.js')
+    .then(registration => {
+      // Reference register globally
+      swReg = registration;
+
+      // Check if subscription exists, update UI accordingly
+      swReg.pushManager.getSubscription().then(setSubscribedStatus);
+    })
+    .catch(console.error);
+}
