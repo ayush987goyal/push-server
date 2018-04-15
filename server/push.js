c489ef076b0ec3c635ba0e62a25be8732553d940
Ayush Goyal <ayush987goyal@gmail.com>
2018-04-16 00:51:59 +0530

    added persistance to subscriptions

--- server/push.js
+++ server/push.js
@@ -1,11 +1,13 @@
 const webpush = require('web-push');
 const urlsafeBase64 = require('urlsafe-base64');
+const Storage = require('node-storage');
 
 // Vapid keys
 const vapid = require('./vapid.json');
 
 // Subscriptions
-let subscriptions = [];
+const store = new Storage(`${__dirname}/db`);
+let subscriptions = store.get('subscriptions') || [];
 
 // Create URL safe vapid  public key
 module.exports.getKey = () => urlsafeBase64.decode(vapid.publicKey);
@@ -15,5 +17,6 @@ module.exports.addSubscription = subscription => {
   // Add to subscriptions array
   subscriptions.push(subscription);
 
-  console.log(subscriptions);
+  // Persist subscriptions
+  store.put('subscriptions', subscriptions);
 };
